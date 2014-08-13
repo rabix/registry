9e1d9a74304577ea14c74c51d5eac508d5cec3d8
Milica Kadic <milica.kadic@sbgenomics.com>
2014-08-13 17:31:47 +0200

    gitHub auth - user update

--- server/app/controllers/user.js
+++ server/app/controllers/user.js
@@ -13,13 +13,7 @@ router.get('/user', function (req, res, next) {
     res.json({user: req.user});
 });
 
-router.get('/user/token', ensureAuthenticated, function (req, res, next) {
-    User.find({})
+router.get('/user/token', function (req, res, next) {
+    User.find({});
     res.json({user: req.user});
 });
-
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.statusCode = 400;
-    res.json({message: 'You must be logged in!'})
-}
