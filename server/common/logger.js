7593ca1660739f0e8c9b22dbd7b07c4ee072bac5
Filip Jelic <filip.jelic@sbgenomics.com>
2014-08-21 16:23:57 +0200

    added /logs folder to gitignore, added all app loging there

--- server/common/logger.js
+++ server/common/logger.js
@@ -2,17 +2,21 @@
  * Created by filip on 8/19/14.
  */
 var winston = require('winston');
+var path = require('path');
 
-var log_dir = '/var/log';
+var rootPath = path.normalize(__dirname + '/..');
+
+var debugLog = rootPath + '/logs/rabix-debug.log';
+var exceptionLog = rootPath + '/logs/rabix-exceptions.log';
 
 var logger = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)({ json: true, timestamp: true }),
-        new winston.transports.File({ filename: log_dir + '/rabix-debug.log', json: false })
+        new winston.transports.File({ filename: debugLog, json: false })
     ],
     exceptionHandlers: [
         new (winston.transports.Console)({ json: true, timestamp: true }),
-        new winston.transports.File({ filename: log_dir + '/rabix-exceptions.log', json: false })
+        new winston.transports.File({ filename: exceptionLog, json: false })
     ],
     exitOnError: false
 });
