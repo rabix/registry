3a528b39716fc6be0d6ac1710a94e37fbd387ed9
Filip Jelic <filip.jelic@sbgenomics.com>
2014-08-21 15:47:26 +0200

    changed log file names

--- server/common/logger.js
+++ server/common/logger.js
@@ -3,16 +3,16 @@
  */
 var winston = require('winston');
 
-var log_dir = '/var/log/rabix';
+var log_dir = '/var/log';
 
 var logger = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)({ json: true, timestamp: true }),
-        new winston.transports.File({ filename: log_dir + '/debug.log', json: false })
+        new winston.transports.File({ filename: log_dir + '/rabix-debug.log', json: false })
     ],
     exceptionHandlers: [
         new (winston.transports.Console)({ json: true, timestamp: true }),
-        new winston.transports.File({ filename: log_dir + '/exceptions.log', json: false })
+        new winston.transports.File({ filename: log_dir + '/rabix-exceptions.log', json: false })
     ],
     exitOnError: false
 });
