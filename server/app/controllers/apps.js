99776555d7f7016b444758e9d3c06c6e293b37c3
flipio <jelic.filip@gmail.com>
2014-09-04 17:48:22 +0200

    Changed app schema, adding new apps now possible

--- server/app/controllers/apps.js
+++ server/app/controllers/apps.js
@@ -4,11 +4,15 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var _ = require('lodash');
+
 var App = mongoose.model('App');
+var Repo = mongoose.model('Repo');
+
 var filters = require('../../common/route-filters');
 
 module.exports = function (app) {
     app.use('/api', router);
+
 };
 
 router.get('/apps', function (req, res, next) {
@@ -32,7 +36,7 @@ router.get('/apps', function (req, res, next) {
     App.count(where).exec(function(err, total) {
         if (err) { return next(err); }
 
-        App.find(where).skip(skip).limit(limit).populate('repoId').exec(function(err, apps) {
+        App.find(where).skip(skip).limit(limit).exec(function(err, apps) {
             if (err) { return next(err); }
 
             res.json({list: apps, total: total});
@@ -44,7 +48,7 @@ router.get('/apps', function (req, res, next) {
 
 router.get('/apps/:id', function (req, res, next) {
 
-    App.findById(req.params.id).populate('repoId').exec(function(err, app) {
+    App.findById(req.params.id).exec(function(err, app) {
         if (err) { return next(err); }
 
         res.json({data: app});
@@ -58,11 +62,44 @@ router.put('/apps', filters.authenticated, function (req, res, next) {
 });
 
 
-router.post('/apps', filters.authenticateClient, function (req, res, next) {
+router.post('/apps', filters.authenticated, function (req, res, next) {
+    var data = req.body;
+
+    // TODO: Validate app JSON
+//    Validator.validateApp(data);
+
+
+    var desc = data.softwareDescription;
+
+    var app = new App();
+
+    app = _.extend(app, data);
+
+    app.name = desc.name;
+    app.description = desc.description;
+    app.author = data.documentAuthor;
+    app.json = data;
+    app.links = {
+        json: ''
+    };
+
+    app.repo_name = desc.repo_name || '';
+    app.repo_owner = desc.repo_owner || '';
+
+    Repo.findOne({'name': desc.repo_name, 'owner': desc.repo_owner}, function (err, repo) {
+
+        if (repo) {
+            app.repo_id = repo._id;
+        }
+
+        app.save();
+    });
+
+    res.json(app);
 
 });
 
-router.delete('/apps', filters.authenticateClient, function (req, res, next) {
+router.delete('/apps', filters.authenticated, function (req, res, next) {
 
 });
 
