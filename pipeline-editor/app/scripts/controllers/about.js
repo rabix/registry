b526d9587bd22887571afee128004dfb40724183
flipio <jelic.filip@gmail.com>
2014-09-16 17:25:06 +0200

    added scaffold for pipeline editor

---
+++ pipeline-editor/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pipelineEditorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pipelineEditorApp
+ */
+angular.module('pipelineEditorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,7 @@ var config = {
         db: 'mongodb://localhost/server-development',
         clientPath: '../../client/app',
         clichePath: '../../cliche/app',
+        pipelineEditorPath: '../../pipeline-editor/app',
         github: {
             clientId: 'bffb0517d8c629f2b2db',
             clientSecret: '3697d0796cb2ed6443495399a963eed96d47bea5',
@@ -37,6 +38,7 @@ var config = {
         db: 'mongodb://localhost/server-test',
         clientPath: '../../client/app',
         clichePath: '../../../cliche/app',
+        pipelineEditorPath: '../../pipeline-editor/app',
         github: {
             clientId: 'bffb0517d8c629f2b2db',
             clientSecret: '3697d0796cb2ed6443495399a963eed96d47bea5',
@@ -62,6 +64,7 @@ var config = {
         db: 'mongodb://localhost/server-production',
         clientPath: '../../client/dist',
         clichePath: '../../cliche/dist',
+        pipelineEditorPath: '../../pipeline-editor/app',
         github: {
             clientId: '8747f41e5e0389547e6d',
             clientSecret: 'bb7770b1835087947e37bba3f39b6ad9e5ca6c85',
