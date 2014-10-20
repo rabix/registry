d2196112d578351cb6bc5e275e4d6b2b9238bf14
Milica Kadic <milica.kadic.87@gmai.com>
2014-10-20 14:55:46 +0200

    preloading some of the images

---
+++ client/app/scripts/directives/preload.js
@@ -0,0 +1,17 @@
+/**
+ * Author: Milica Kadic
+ * Date: 10/20/14
+ * Time: 14:26 PM
+ */
+'use strict';
+
+angular.module('registryApp')
+    .directive('preload', ['$templateCache', function ($templateCache) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {},
+            template: $templateCache.get('views/partials/preload.html'),
+            link: function() {}
+        };
+    }]);
