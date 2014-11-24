758a06f0c5af71f46fe1ea9c3eea4ad9cfb9c74e
Filip Jelic <filip.jelic@sbgenomics.com>
2014-11-24 17:04:54 +0100

    Removed route in pipeline used for testing

--- server/pipeline/formater.js
+++ server/pipeline/formater.js
@@ -2,6 +2,13 @@
 
 var _ = require('lodash');
 
+/**
+ * Formatter for pipeline schemas
+ *
+ * @public_methods {toRabixSchema}, {toPipelineSchema}
+ * @type {{packedSchema: null, toRabixSchema: toRabixSchema, toPipelineSchema: toPipelineSchema, _transformRelationsToSteps: _transformRelationsToSteps, _generateSystemNodes: _generateSystemNodes, _checkSystem: _checkSystem, _attachOutput: _attachOutput, _createInOut: _createInOut, _createOneAppStep: _createOneAppStep, _transformStepsToRelations: _transformStepsToRelations}}
+ */
+
 var formater = {
 
     packedSchema: null,
@@ -11,13 +18,12 @@ var formater = {
         var json = _.clone(j);
         // reset schema
         this.packedSchema = {};
+        this.packedSchema.steps = [];
         this.packedSchema.inputs = {};
         this.packedSchema.outputs = {};
 
         if ( (!json.relations || json.relations.length === 0 ) && json.nodes.length === 1) {
             var _id = json.nodes[0]._id;
-
-            this.packedSchema.steps = [];
 
             this.packedSchema.steps.push({
                 _id: _id,
@@ -43,21 +49,20 @@ var formater = {
 
         // reset schema
         this.packedSchema = {};
+        this.packedSchema.schemas = {};
+        this.packedSchema.nodes = [];
+        this.packedSchema.relations = [];
+        this.packedSchema.display = json.display;
 
-        this._transformStepsToRelations(json.steps);
+        this._transformStepsToRelations(json);
 
-        delete json.steps;
-        json.relations = this.packedSchema.relations;
-
-        return json;
+        return this.packedSchema;
 
     },
 
     _transformRelationsToSteps: function (relations, nodes, schemas) {
 
         var _self = this;
-
-        this.packedSchema.steps = [];
 
         _.forEach(relations, function (rel) {
             var step, node_schema;
@@ -165,15 +170,21 @@ var formater = {
         return step;
     },
 
-    _transformStepsToRelations: function (steps) {
+    _transformStepsToRelations: function (json) {
 
-        var relations = [];
+        var steps = json.steps,
+            relations = this.packedSchema.relations,
+            nodes = this.packedSchema.nodes,
+            schemas = this.packedSchema.schemas;
 
         _.forEach(steps, function (step) {
             var end_node = step._id, input_name, output_name, start_node;
 
+            schemas[step._id] = step.app;
+            nodes.push(step.app);
+
             _.forEach(step.inputs, function (from, input) {
-                var relation, s;
+                var relation, s, filter;
 
                 s = from.$from.split('.');
 
@@ -181,7 +192,23 @@ var formater = {
                     start_node = s[0];
                     output_name = s[1];
                 } else {
+                    filter = _.filter(json.inputs, function (input) {
 
+                        return input.outputs.properties[s[0]];
+                    });
+
+                    if (filter.length !== 0) {
+
+                        schemas[filter[0].id] = filter[0];
+                        nodes.push(filter[0]);
+
+                        start_node = filter[0].id;
+                    } else {
+                        start_node = '';
+                        throw new Error('Invalid output name');
+                    }
+
+                    output_name = s[0];
                 }
 
                 input_name = input;
@@ -199,9 +226,44 @@ var formater = {
                 relations.push(relation);
             });
 
+            _.forEach(step.outputs, function (to, output) {
+                var relation, filter;
+
+                start_node = end_node;
+                output_name = output;
+
+                input_name = to.$to;
+                filter = _.filter(json.outputs, function (out) {
+
+                    return out.inputs.properties[input_name];
+                });
+
+                if (filter.length !== 0) {
+                    schemas[filter[0].id] = filter[0];
+                    nodes.push(filter[0]);
+
+                    end_node = filter[0].id;
+                } else {
+                    end_node = '';
+                    throw new Error('Invalid Output name');
+                }
+
+                relation = {
+                    end_node: end_node,
+                    input_name: input_name,
+                    output_name: output_name,
+                    start_node: start_node,
+                    type: 'connection',
+                    // id needs to be a string
+                    id: _.random(100000, 999999) + ''
+                };
+
+                relations.push(relation);
+
+            });
+
         });
 
-        this.packedSchema.relations = relations;
     }
 
 };
