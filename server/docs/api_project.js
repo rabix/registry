c13847b29a676cfa775b672fad936cbc7e244819
Filip Jelic <filip.jelic@sbgenomics.com>
2014-12-24 16:37:32 +0100

    moved docs generation to grunt, added apidoc.json conf

--- server/docs/api_project.js
+++ server/docs/api_project.js
@@ -1,12 +1,14 @@
 define({
-  "name": "Rabix",
-  "version": "0.0.1",
-  "description": "",
+  "name": "Rabix Registry",
+  "version": "0.1.0",
+  "description": "Rabix Registry API documentation",
+  "title": "Rabix Registry",
+  "url": "https://www.rabix.org/docs",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2014-12-23T16:25:44.150Z",
+    "time": "2014-12-24T15:32:37.308Z",
     "url": "http://apidocjs.com",
     "version": "0.12.0"
   }
