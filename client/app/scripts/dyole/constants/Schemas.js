bb19e4ccd98ccb33c0856b583301bc866a367472
Filip Jelic <filip.jelic@sbgenomics.com>
2015-02-20 17:01:20 +0100

    Separated dyole constants

---
+++ client/app/scripts/dyole/constants/Schemas.js
@@ -0,0 +1,29 @@
+/**
+ * Created by filip on 2/20/15.
+ */
+'use strict';
+angular.module('registryApp.dyole')
+    .constant('rawPipeline', {
+        display: {
+            canvas: {
+                x: 0,
+                y: 0,
+                zoom: 1
+            },
+            nodes: {}
+        },
+        nodes: [],
+        relations: [],
+        schemas: {}
+    })
+    .constant('systemNodeModel', {
+        '@id': '',
+        'label': 'System app',
+        'softwareDescription': {
+            'repo_owner': 'rabix',
+            'repo_name': 'system',
+            'type': ''
+        },
+        'inputs': [],
+        'outputs': []
+    });
--- client/app/scripts/dyole/dyole.config.js
+++ client/app/scripts/dyole/dyole.config.js
@@ -6,28 +6,4 @@
 
 'use strict';
 
-angular.module('registryApp.dyole', [])
-    .constant('rawPipeline', {
-        display: {
-            canvas: {
-                x: 0,
-                y: 0,
-                zoom: 1
-            },
-            nodes: {}
-        },
-        nodes: [],
-        relations: [],
-        schemas: {}
-    })
-    .constant('systemNodeModel', {
-        '@id': '',
-        'label': 'System app',
-        'softwareDescription': {
-            'repo_owner': 'rabix',
-            'repo_name': 'system',
-            'type': ''
-        },
-        'inputs': [],
-        'outputs': []
-    });
+angular.module('registryApp.dyole', []);
