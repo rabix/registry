35e776ffb898eb32aa9ed4ba5be1f3cb9ae9ac93
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-03-30 16:06:34 +0200

    Integrated markdown to help message component.

--- client/app/scripts/common/directives/help-message.js
+++ client/app/scripts/common/directives/help-message.js
@@ -10,9 +10,54 @@ angular.module('registryApp.common')
                 message: '='
             },
             restrict: 'E',
-            template: '<i ng-if="showButton" class="fa fa-question help-button" popover-animation="true" popover-placement="right" popover="{{ ::message }}"></i>',
-            link: function(scope) {
+            replace: true,
+            template:   '<div class="help-message">' +
+                            '<i ng-if="showButton" class="fa fa-question help-button"></i>' +
+                            '<help-message-popover></help-message-popover>' +
+                        '</div>',
+            link: function(scope, element) {
+
                 scope.showButton = !_.isEmpty(scope.message);
+                scope.showPopup = false;
+                console.log(scope.showPopup);
+
+                function handler () {
+                    scope.showPopup = !scope.showPopup;
+                    scope.$apply();
+                }
+
+                angular.element(element).on('click', '.help-button', handler);
+
+                scope.$on('$destroy', function() {
+                    angular.element(element).off('click', '.help-button', handler);
+                });
+            }
+        };
+    }])
+    .directive('helpMessagePopover', ['$templateCache', '$document', function ($templateCache, $document) {
+        return {
+            restrict: 'EA',
+            replace: true,
+            template: $templateCache.get('views/partials/help-message-popover.html'),
+            link: function(scope) {
+                function closeHandler (e) {
+                    var target = $(e.target);
+
+                    // hide popover only if click is outside help message directive
+                    if (scope.showPopup && target.parents('.help-message').length === 0) {
+                        scope.showPopup =  false;
+                        scope.$apply();
+                    }
+                }
+
+                // remove event listener when popover isn't visible
+                scope.$watch('showPopup', function(n) {
+                    if (n) {
+                        $document.on('click', closeHandler);
+                    } else {
+                        $document.off('click', closeHandler);
+                    }
+                });
             }
         };
     }]);
