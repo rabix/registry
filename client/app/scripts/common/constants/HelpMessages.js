90e93724fced17bc9ff9176dca99c500339cc5eb
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-03-30 16:06:34 +0200

    Integrated markdown to help message component.

--- client/app/scripts/common/constants/HelpMessages.js
+++ client/app/scripts/common/constants/HelpMessages.js
@@ -5,7 +5,7 @@
 angular.module('registryApp.common').constant('HelpMessages', {
     cliche: {
         general: {
-            environment: 'env',
+            environment: 'An h1 header\n============\n\nParagraphs are separated by a blank line.\n\n2nd paragraph. *Italic*, **bold**, and `monospace`. Itemized lists\nlook like:\n\n  * this one\n  * that one\n  * the other one\n\nNote that --- not considering the asterisk --- the actual text\ncontent starts at 4-columns in.\n\n> Block quotes are\n> written like so.\n>\n> They can span multiple paragraphs,\n> if you like.\n',
             resources: '',
             command: '',
             arguments: ''
