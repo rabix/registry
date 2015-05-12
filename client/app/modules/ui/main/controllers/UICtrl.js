38b3a7281389e17e03867647598e54f94de9aa39
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-05-12 12:18:41 +0200

    Rabix checkbox directive

--- client/app/modules/ui/main/controllers/UICtrl.js
+++ client/app/modules/ui/main/controllers/UICtrl.js
@@ -2,9 +2,17 @@
  * Created by filip on 11.5.15..
  */
 
+'use strict';
+
 angular.module('registryApp.ui')
     .controller('UICtrl', ['$scope', function ($scope) {
         $scope.testClick = function () {
             console.log('click');
-        }
+        };
+
+        $scope.checkboxModel = true;
+
+        $scope.disableCheckbox = function () {
+            $scope.disable = !$scope.disable;
+        };
     }]);
