c7814fe6b62bfb54244770bacdf04e9bb567b666
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-05-25 12:51:21 +0200

    Extended rbTabs directive to work with ui-sref and accept configuration array

--- client/app/modules/ui/tabs/rbTabs.js
+++ client/app/modules/ui/tabs/rbTabs.js
@@ -5,7 +5,7 @@
 
 
 angular.module('registryApp.ui')
-    .directive('rbTabs', [function() {
+    .directive('rbTabs', ['$state', '$stateParams', function($state, $stateParams) {
         /**
          * @ngdoc directive
          * @name rbTabs
@@ -45,48 +45,101 @@ angular.module('registryApp.ui')
          *      [tab-src="path/to/tab/templates"]>
          *  <rb-tabs>
          *
+         * @todo: fix problem with scope when passing path to templates
          *
-         * @param {string} tabs List of tab names
+         * @param {string=} tabs List of tab names
+         * @param {object=} tab-options Array with tabs config:
+         *      [
+         *          {
+         *              name: 'nameOfTab',
+         *              slug: 'tabHeading',
+         *              uiSref: 'linkOfTab',
+         *          }
+         *      ]
+         *
          * @param {string=} tab-src Path to directory which contains tab templates
          * @param {string=} active-tab Default active tab, otherwise the first tab in the list will be active
+         * @param {string=} tab-classes List of classes to be applied to each individual tab
+         * @param {string=} container-classes List of classes to be applied to tab content container
+         * @param {string=} state-param Which `$stateParam` should directive listen to for the active tab
+         * @param {boolean=} heading Applies heading classes, defaults to true
+         * @param {boolean=} page Applies nav-page class, defaults to false
          * @param {Function=} tab-callback Function on controller scope which is to be called when tab is switched
          *
          */
 
         function postLink (scope, element, attr) {
+            scope.heading = typeof attr.heading === 'undefined' || attr.heading === 'true';
+            scope.page = attr.page === 'true';
 
-            scope.tabs = attr.tabs.split(', ');
+            if (attr.tabs && !scope.tabOptions) {
+                var tabs = attr.tabs.split(', ');
+                scope.tabOptions = _.map(tabs, function(tab) {
+                    return {
+                        name: tab,
+                        slug: tabSlugify(tab)
+                    };
+                });
+            } else if (!scope.tabOptions) {
+                throw Error('Please supply either tab-options or tabs attributes to rbTabs directive.');
+            }
 
             if (attr.tabSrc) {
-                scope.tabsMap = {};
+                scope.hasSrc = true;
                 if (_.last(attr.tabSrc) !== '/') { attr.tabSrc += '/'; }
 
-                _.forEach(scope.tabs, function (tab) {
-                    scope.tabsMap[tab] = formatTemplateUrl(tab, attr.tabSrc);
+                _.forEach(scope.tabOptions, function (tab) {
+                    tab.src = formatTemplateUrl(tab, attr.tabSrc);
                 });
             }
 
-            scope.activeTab = attr.activeTab || scope.tabs[0];
+            scope.activeTab = attr.activeTab || $stateParams[attr.stateParam] || scope.tabOptions[0].slug;
+
 
             scope.activateTab = function (tab) {
-                scope.activeTab = tab;
+                if (!attr.stateParam) {
+                    scope.activeTab = tab.slug;
+                }
 
                 if (typeof scope.tabCallback === 'function') {
-                    scope.tabCallback({tab: tab});
+                    scope.tabCallback({tab: tab.slug});
+                }
+
+                if (tab.state) {
+                    $state.go(tab.state, tab.params || {});
                 }
             };
         }
 
+        function tabSlugify (tab) {
+            return tab.toLowerCase().replace(/\s+/g, '-');
+        }
+
         function formatTemplateUrl (tab, src) {
-            return src + tab.toLowerCase().replace(/\s+/g, '-') + '.html';
+            return src + tabSlugify(tab) + '.html';
         }
 
         return {
             restrict: 'E',
             scope: {
-                tabCallback: '&'
+                tabCallback: '&',
+                tabOptions: '=?'
             },
-            link: postLink,
-            templateUrl: 'modules/ui/tabs/default.html'
+            replace: true,
+            templateUrl: 'modules/ui/tabs/default.html',
+            compile: function(element, attr) {
+                console.log('compiling again');
+                
+
+                if (attr.tabClasses) {
+                    $(element).find('.tabs-inner').addClass(attr.tabClasses);
+                }
+
+                if (attr.containerClasses) {
+                    $(element).find('.tabs-container').addClass(attr.containerClasses);
+                }
+
+                return postLink;
+            }
         };
     }]);
