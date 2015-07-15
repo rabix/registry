c6f4d7a85eec3546edd77af8dce10d7ebe6b3fc1
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-07-15 15:01:57 +0200

    Made a list of things that need to be copied over

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,4 +2,62 @@ var gulp = require('gulp');
 
 gulp.task('default', function() {
 
+    /*
+
+    watch task that watches:
+        -sass
+        -gulpfile
+        -templates
+
+    connect:
+        connects to localhost
+        does livereload
+        dist
+        docs
+
+    ngdocs:
+        for generating documentation
+
+    jshint:
+        -checks all js files
+        -checks test files
+
+    clean:
+        -clears out dist
+
+
+    autoprefixer
+
+    wiredep
+
+    sass
+
+    filerev
+
+    useminPrepare
+
+    usemin
+
+    cssmin
+
+    concat
+
+    imagemin
+
+    svgmin
+
+    htmlmin (don't need this? Using ng-templates)
+
+    ng-min (minifies angular's long form dependency injection)
+
+    ng-annotate
+
+    copy
+
+    concurrent
+
+    ng-templates
+
+
+    */
 });
