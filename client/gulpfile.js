bde4a0f49b76475b3da090d3ddbe6f4856133b43
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-07-16 12:43:39 +0200

    found dependencies

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -17,46 +17,66 @@ gulp.task('default', function() {
 
     ngdocs:
         for generating documentation
+        https://www.npmjs.com/package/gulp-ngdocs
 
     jshint:
         -checks all js files
         -checks test files
+        https://github.com/spalger/gulp-jshint
 
     clean:
         -clears out dist
 
+         gulp.task('clean', function (done) {
+            $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')], done);
+         });
+
 
     autoprefixer
+        https://www.npmjs.com/package/gulp-autoprefixer
+
 
     wiredep
+        https://github.com/taptapship/wiredep
 
     sass
+        libsass
 
     filerev
+        https://www.npmjs.com/package/gulp-rev
+
 
     useminPrepare
 
     usemin
-
+        https://www.npmjs.com/package/gulp-usemin
     cssmin
 
     concat
+        https://github.com/wearefractal/gulp-concat
 
     imagemin
+        https://github.com/sindresorhus/gulp-imagemin
 
     svgmin
 
     htmlmin (don't need this? Using ng-templates)
+        https://www.npmjs.com/package/gulp-minify-html
 
     ng-min (minifies angular's long form dependency injection)
-
+        https://github.com/sindresorhus/gulp-ngmin
     ng-annotate
-
+        https://www.npmjs.com/package/gulp-ng-annotate
     copy
+        got copying over other files (assets) that have not been taken care of by the other tasks
+        for example htaccess
+        images
+        fonts
+        special scripts that need to be loaded directly to index.html
 
-    concurrent
 
     ng-templates
+        https://www.npmjs.com/package/gulp-ng-templates
 
 
     */
