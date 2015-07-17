5127f05fe0cedc7d83d6c540b8fb2c97e6f9af0f
Filip Jelic <jelic.filip@gmail.com>
2015-07-17 14:17:27 +0200

    fix

--- client/app/modules/dyole/factories/common.js
+++ client/app/modules/dyole/factories/common.js
@@ -68,6 +68,10 @@ angular.module('registryApp.dyole')
                 var _id, check = true, name = (model.softwareDescription && model.softwareDescription.label) ? model.softwareDescription.label : model.label || model.name,
                     n = 0;
 
+                if (name.charAt(0) === '#') {
+                    name = name.slice(1);
+                }
+
                 var _fixName = function (n) {
 
                     var regex = /[^A-Za-z0-9]/g;
