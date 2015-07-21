f606447fff25b3732696eaf5dc06419d0783e7a8
branko7171 <branko.vlajin@sbgenomics.com>
2015-07-21 12:18:33 +0200

    Timeout fix. Renamed.

--- client/app/modules/common/directives/equals.js
+++ client/app/modules/common/directives/equals.js
@@ -4,11 +4,11 @@
 'use strict';
 
 angular.module('registryApp.common')
-.directive('equals', function () {
+.directive('isUnequal', function ($timeout) {
     return {
         require: '?ngModel',
         scope :{
-          equals: '=',
+          other: '=',
             own: '='
         },
         link: function(scope, element, attrs,ngModel, ctrl) {
@@ -20,17 +20,21 @@ angular.module('registryApp.common')
             });
 
             // observe the other value and re-validate on change
-            scope.$watch('equals', function (val) {
+            scope.$watch('other', function (val) {
                 validate();
             });
 
             var validate = function() {
-                // values
-                var val1 = scope.own;
-                var val2 = scope.equals;
+                $timeout(function(){
+                    // values
+                    var val1 = scope.own;
+                    var val2 = scope.other;
+                    var value = (val1 !== val2);
+                    // set validity
+                    ngModel.$setValidity('isUnequal', value);
+                },0);
 
-                // set validity
-                ngModel.$setValidity('equals', val1 !== val2);
+
             };
         }
     };
