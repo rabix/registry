a6ab5724f5b1003344e29ad1d68c8bbdf13116e1
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2015-07-28 10:47:31 +0200

    Work on inject task, still not functioning properly.

--- client/gulp/inject.js
+++ client/gulp/inject.js
@@ -7,6 +7,8 @@ var config = require('./config').config;
 var path = require('path');
 var errorHandler = require('./config').errorHandler;
 
+var _ = require('lodash');
+
 var $ = require('gulp-load-plugins')();
 
 /**
@@ -15,51 +17,100 @@ var $ = require('gulp-load-plugins')();
  * Should run:
  *  - on new js or css file creation
  */
-gulp.task('inject', function () {
-    //var injectStyles = gulp.src([
-    //    path.join(config.app, '/**/*.css')
-    //], { read: false });
-    var injectBower = gulp.src([
-            path.join(config.bowerDir, '/**/*.js')
-        ],
-        {read: false}
-        //{
-        //    starttag: '<!-- build:js({.tmp,app}) modules/vendor.js -->',
-        //    endtag: '<!-- endbuild -->'
-        //}
-    );
+var target = gulp.src(path.join(config.app, 'index.html'));
 
-    var injectCliche = gulp.src(path.join(config.app, 'modules/cliche/**/*.js'))
-        .pipe($.angularFilesort()).on('error', errorHandler('AngularFilesort'));
+var baseConfig = {
+    relative: true,
+    addRootSlash: false
+};
 
-    var clicheOptions = {
-        starttag: '<!-- inject:cliche:{{ext}} -->',
-        relative: true,
-        addRootSlash: false,
-        transform: function (filepath, file, i, length) {
-            console.log(filepath, file, i, length);
-            return 'asdf';
-        }
 
-    };
-    var injectScripts = gulp.src([
-        path.join(config.app, '/modules/**/*.js')
-        //path.join(config.bowerDir, '**/*.js')
-    ])
-        .pipe($.angularFilesort()).on('error', errorHandler('AngularFilesort'));
-    //
-    var injectOptions = {
-        ignorePath: [config.app, path.join(config.tmp, '/serve')],
-        addRootSlash: false,
-        relative: true
-    };
+gulp.task('inject:modules', function () {
+    var modulesSource = gulp.src([
+        path.join(config.scriptsPath, '**/*.js'),
+        '!' + path.join(config.scriptsPath, 'dyole/**/*.js'),
+        '!' + path.join(config.scriptsPath, 'cliche/**/*.js'),
+        '!' + path.join(config.scriptsPath, 'ui/**/*.js')
+    ]).pipe($.angularFilesort());
 
-    return gulp.src(path.join(config.app, '/index.html'))
-        .pipe($.debug({title: 'inject'}))
-        .pipe($.inject(injectCliche), clicheOptions)
-        //.pipe($.inject(injectScripts), injectOptions)
-        //.pipe($.inject(injectBower), injectOptions)
-        .pipe(gulp.dest(path.join(config.app)));
+    return target.pipe($.inject(modulesSource, _.extend(baseConfig, {
+            starttag: '<!-- inject:modules:{{ext}} -->',
+            ignorePath: [
+                'cliche',
+                'dyole',
+                'ui'
+            ]
+        })))
+        .pipe(gulp.dest(config.app));
+});
+
+gulp.task('inject:dyole', ['inject:cliche'], function() {
+    var dyoleSource = gulp.src(path.join(config.scriptsPath, 'dyole/**/*.js'))
+        .pipe($.angularFilesort());
+
+    return target.pipe($.inject(dyoleSource, _.extend(baseConfig, {
+            starttag: '<!-- inject:dyole:{{ext}} -->'
+        })))
+        .pipe(gulp.dest(config.app));
+});
+
+gulp.task('inject:cliche', function() {
+    var clicheSource = gulp.src(path.join(config.scriptsPath, 'cliche/**/*.js'))
+        .pipe($.angularFilesort());
+
+    return target.pipe($.inject(clicheSource, _.extend(baseConfig, {
+        starttag: '<!-- inject:cliche:{{ext}} -->'
+    })))
+        .pipe(gulp.dest(config.app));
+});
+
+
+gulp.task('inject:ui', function() {
+    var uiSource = gulp.src(path.join(config.scriptsPath, 'ui/**/*.js'))
+        .pipe($.angularFilesort());
+
+    return target.pipe($.inject(uiSource, _.extend(baseConfig, {
+        starttag: '<!-- inject:ui:{{ext}} -->'
+    })))
+        .pipe(gulp.dest(config.app));
+});
+
+gulp.task('inject',
+    [
+        //'inject:dyole',
+        //'inject:cliche',
+        'inject:ui'
+    ],
+    function () {
+
+    var cssSource = gulp.src([
+        path.join(config.sassPath, 'main.css'),
+        path.join(config.bowerDir, 'codemirror/lib/codemirror.css'),
+        path.join(config.bowerDir, 'codemirror/theme/mbo.css'),
+        path.join(config.bowerDir, 'github-markdown-css/github-markdown.css'),
+        path.join(config.bowerDir, 'angular-ui-notification/dist/angular-ui-notification.min.css'),
+        path.join(config.bowerDir, 'nvd3/nv.d3.css')
+    ]);
+
+    var codemirrorSource = gulp.src([
+        path.join(config.bowerDir, 'codemirror/lib/codemirror.js'),
+        path.join(config.bowerDir, 'codemirror/mode/javascript/javascript.js')
+    ], {read: false});
+
+    var vendorSource = gulp.src([
+        path.join(config.scriptsPath, 'vendor/**/*.js'),
+        path.join(config.bowerDir, '/**/*.min.js')
+    ], {read: false});
+
+    return target.pipe($.inject(cssSource, baseConfig))
+
+        .pipe($.inject(vendorSource, _.extend(baseConfig, {
+            starttag: '<!-- inject:vendor:{{ext}} -->'
+        })))
+        .pipe($.inject(codemirrorSource, _.extend(baseConfig, {
+            starttag: '<!-- inject:codemirror:{{ext}} -->'
+        })))
+        .pipe(gulp.dest(config.app));
 });
 
 
